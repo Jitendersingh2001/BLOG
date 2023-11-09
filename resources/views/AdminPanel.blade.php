e53745b2c9161e1c87e6e0b25bb708ac16a454a0
Jitender singh <jitendersinghjatt09@gmail.com>
2023-11-09 23:19:57 +0530

    done

---
+++ resources/views/AdminPanel.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phoenix Blog Admin Panel</title>
+     <!-- CSS LINK -->
+     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <!-- TOAST CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.css" integrity="sha512-8D+M+7Y6jVsEa7RD6Kv/Z7EImSpNpQllgaEIQAtqHcI0H6F4iZknRj0Nx1DCdB+TwBaS+702BGWYC0Ze2hpExQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+<!-- JQUERY CDN LINK -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <!-- TOAST JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js" integrity="sha512-zlWWyZq71UMApAjih4WkaRpikgY9Bz1oXIW5G0fED4vk14JjGlQ1UmkGM392jEULP8jbNMiwLWdM8Z87Hu88Fw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- JAVASCRIPT/JQUERY LINK -->
+<script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
+</html>
